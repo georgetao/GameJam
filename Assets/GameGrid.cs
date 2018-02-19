fbe076737c8f413c182e6e50785e520c55dbea28
Kevin <kevin.qi@berkeley.edu>
2018-02-18 18:29:36 -0800

    Bug fixes for Falling

--- Assets/GameGrid.cs
+++ Assets/GameGrid.cs
@@ -2,35 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour {
+public class GameGrid : MonoBehaviour
+{
     public static int w = 4;
     public static int h = 12;
     public static Transform[,] grid = new Transform[w, h];
     public static Dictionary<Group, int> groups;
+    private int timeToFall = 0;
+    public int deltaTime=1;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         groups = new Dictionary<Group, int>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Time.time > timeToFall)
+        {
+            fall();
+            timeToFall += deltaTime;
+        }
+
+    }
 
     void fall()
     {
         for (int x = 0; x < w; x++)
         {
-            for (int y = 0; y < h; y++)
+            for (int y = 1; y < h; y++)
             {
-                if (grid[x, y] != null && isFree(grid[x,y]))
+                
+                if (grid[x, y] != null && isFree(grid[x, y]))
                 {
+                    //Debug.Log(grid[x, y]);
                     // Move one towards bottom
+                    Debug.Log(y);
                     grid[x, y - 1] = grid[x, y];
                     grid[x, y] = null;
 
                     // Update Block position
-                    grid[x, y - 1].position += new Vector3(0, -1, 0);
+                    grid[x, y - 1].position += new Vector3(0f, -1, 0);
                 }
             }
         }
