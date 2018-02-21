fda790f2f057ab8f0461df8a4c5b98376fc40f7d
Kevin <kevin.qi@berkeley.edu>
2018-02-21 00:28:58 -0800

    Fixed stacking problem

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -39,35 +39,20 @@ public class Grid : MonoBehaviour
         {
             fall();
             timeToFall = timeReset;
-            if (timeReset > 0.5f)
+            if (timeReset > 0.2f)
             {
-                timeReset -= 0.1f;
+                timeReset -= 0.01f;
             }
         }
     }
 
     void fall()
     {
-        for (int x = 0; x < w; x++)
-        {
-            for (int y = 1; y < h; y++)
-            {
-                if (grid[x, y] != null && isFree(grid[x, y]))
-                {
-                    // Move one towards bottom
-                    grid[x, y - 1] = grid[x, y];
-                    grid[x, y] = null;
-
-                    // Update Block position
-                    grid[x, y - 1].position += new Vector3(0f, -1, 0);
-                } 
-            }
-        }
         foreach (Transform child in transform)
         {
             Vector2 v = child.position;
             grid[(int)v.x, (int)v.y] = child;
-            if (v.y == 0 || grid[(int)v.x, (int)v.y - 1] != null)
+            if (v.y == 0 || grid[(int)v.x, (int)v.y - 1] != null) // Reached lowest point
             {
                 // Transfering the blocks to groups
                 // Essentially everytime a block should go to a group, I delete it from the Grid,
@@ -84,7 +69,15 @@ public class Grid : MonoBehaviour
                     }
                 }
             }
+            else // Still free falling
+            {
+                grid[(int)v.x, (int)v.y - 1] = grid[(int)v.x, (int)v.y];
+                grid[(int)v.x, (int)v.y] = null;
+                child.position += new Vector3(0f, -1, 0);
+
+            }
         }
+        
     }
 
     bool isFree(Transform transform)
